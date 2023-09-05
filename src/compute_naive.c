02b1717b122853a6bca109db8d9658225484d398
flwfdd <1525876733@qq.com>
2023-09-05 17:44:58 +0800

    done

--- src/compute_naive.c
+++ src/compute_naive.c
@@ -1,22 +1,57 @@
 #include "compute.h"
 
 // Computes the dot product of vec1 and vec2, both of size n
-int dot(uint32_t n, int32_t *vec1, int32_t *vec2) {
+int dot(uint32_t n, int32_t *vec1, int32_t *vec2)
+{
   // TODO: implement dot product of vec1 and vec2, both of size n
-
-  return -1;
+  int sum = 0;
+  for (int i = 0; i < n; i++)
+  {
+    sum += vec1[i] * vec2[i];
+  }
+  return sum;
 }
 
 // Computes the convolution of two matrices
-int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix) {
+int convolve(matrix_t *a_matrix, matrix_t *b_matrix, matrix_t **output_matrix)
+{
   // TODO: convolve matrix a and matrix b, and store the resulting matrix in
   // output_matrix
 
-  return -1;
+  // flip matrix b
+  int32_t *b_data = (int32_t *)malloc(sizeof(int32_t) * b_matrix->rows * b_matrix->cols);
+  for (int i = 0; i < b_matrix->rows; i++)
+  {
+    for (int j = 0; j < b_matrix->cols; j++)
+    {
+      b_data[i * b_matrix->cols + j] = b_matrix->data[(b_matrix->rows - i - 1) * b_matrix->cols + b_matrix->cols - j - 1];
+    }
+  }
+
+  *output_matrix = (matrix_t *)malloc(sizeof(matrix_t));
+  (*output_matrix)->rows = a_matrix->rows - b_matrix->rows + 1;
+  (*output_matrix)->cols = a_matrix->cols - b_matrix->cols + 1;
+  (*output_matrix)->data = (int32_t *)malloc(sizeof(int32_t) * ((*output_matrix)->rows) * ((*output_matrix)->cols));
+
+  for (int i = 0; i < (*output_matrix)->rows; i++)
+  {
+    for (int j = 0; j < (*output_matrix)->cols; j++)
+    {
+      (*output_matrix)->data[i * (*output_matrix)->cols + j] = 0;
+      for (int k = 0; k < b_matrix->rows; k++)
+      {
+        (*output_matrix)->data[i * (*output_matrix)->cols + j] += dot(b_matrix->cols, a_matrix->data + (i + k) * a_matrix->cols + j, b_data + k * b_matrix->cols);
+      }
+    }
+  }
+
+  free(b_data);
+  return 0;
 }
 
 // Executes a task
-int execute_task(task_t *task) {
+int execute_task(task_t *task)
+{
   matrix_t *a_matrix, *b_matrix, *output_matrix;
 
   if (read_matrix(get_a_matrix_path(task), &a_matrix))
--- src/coordinator_mpi.c
+++ src/coordinator_mpi.c
@@ -6,12 +6,72 @@
 #define NEW_TASK 1
 #define TERMINATE -1
 
-int main(int argc, char *argv[]) {
-  if (argc < 2) {
+int main(int argc, char *argv[])
+{
+  if (argc < 2)
+  {
     printf("Error: not enough arguments\n");
     printf("Usage: %s [path_to_task_list]\n", argv[0]);
     return -1;
   }
 
   // TODO: implement Open MPI coordinator
+  int num_tasks;
+  task_t **tasks;
+  if (read_tasks(argv[1], &num_tasks, &tasks))
+    return -1;
+
+  MPI_Init(&argc, &argv);
+
+  int procId, totalProcs;
+
+  MPI_Comm_size(MPI_COMM_WORLD, &totalProcs);
+  MPI_Comm_rank(MPI_COMM_WORLD, &procId);
+
+  if (procId == 0)
+  {
+    // manager
+    int nextTask = 0;
+    MPI_Status status;
+    int32_t message;
+
+    while (nextTask < num_tasks)
+    {
+      MPI_Recv(&message, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+      MPI_Send(&nextTask, 1, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD);
+      nextTask++;
+    }
+
+    for (int i = 1; i < totalProcs; i++)
+    {
+      MPI_Recv(&message, 1, MPI_INT, MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &status);
+      message=TERMINATE;
+      MPI_Send(&message, 1, MPI_INT, status.MPI_SOURCE, 0, MPI_COMM_WORLD);
+    }
+  }
+  else
+  {
+    // worker
+    int32_t message;
+
+    while (true)
+    {
+      MPI_Send(&message, 1, MPI_INT, 0, 0, MPI_COMM_WORLD);
+      MPI_Recv(&message, 1, MPI_INT, 0, MPI_ANY_TAG, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+      if (message == TERMINATE)
+        break;
+      else
+      {
+        if (execute_task(tasks[message]))
+        {
+          printf("Task %d failed\n", message);
+          return -1;
+        }
+        free(tasks[message]->path);
+      }
+    }
+  }
+
+  free(tasks);
+  MPI_Finalize();
 }
